4633089c680fb8ba75c896b9406364129ac79a69
michaelCaleyWhaley <michael.caley@river-island.com>
2017-06-05 10:57:17 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {  
+  consumer_key: '02EUTS3GLWyvG3oxqWtSuAabX',  
+  consumer_secret: 'qgMF9OIMa3zNN95Lo9EcKPIXUUEgFinArkus1JLjXNVQhp61Oe',
+  access_token: '225525650-yKmyTuhRWzWndtgygIG2An92lJrHUEpVaYpz550n',  
+  access_token_secret: 'IFpJdblMjnmXsAWXevgJIr6EiXlOGjVC6b347SoEf89wx'
+}
