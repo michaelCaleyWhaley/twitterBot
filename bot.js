d9885f2f3855d55f1214e99ff43eb5bb22b29d66
michaelCaleyWhaley <michael.caley@river-island.com>
2017-06-06 15:24:04 +0100

    first commit

--- bot.js
+++ bot.js
@@ -11,16 +11,14 @@ var Twitter = new twit(config);
 // find latest tweet according the query 'q' in params
 var retweet = function() {  
     var params = {
-        q: '#Grime4Corbyn',  // REQUIRED
+        q: '#Grime4Corbyn',
         result_type: 'recent',
         lang: 'en'
     }
     Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
         if (!err) {
-          // grab ID of tweet to retweet
             var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
+
             Twitter.post('statuses/retweet/:id', {
                 id: retweetId
             }, function(err, response) {
@@ -34,7 +32,7 @@ var retweet = function() {
                 }
             });
         }
-        // if unable to Search a tweet
+
         else {
           console.log('Something went wrong while SEARCHING...');
         }
